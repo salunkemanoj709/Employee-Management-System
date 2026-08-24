c04f3c24fa5928f8d8984df5887b38569e8a831a
Hi Manoj <salunkemanoj709@gmail.com>
2026-08-24 13:49:26 +0530

    all changes done

--- src/main/java/com/example/demo/StudentApplication.java
+++ src/main/java/com/example/demo/StudentApplication.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-/////allllllllllllllll
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
